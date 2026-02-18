22c54a535eff87ce00cf2b51c314206b7b107132
Wangake <youngwanga254@gmail.com>
2026-02-18 23:39:20 +0300

    Create id.js

---
+++ id.js
@@ -0,0 +1,12 @@
+// id.js - Simple ID generator
+function makeid(length = 4) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+module.exports = { makeid };
